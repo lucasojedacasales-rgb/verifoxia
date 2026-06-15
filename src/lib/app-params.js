2ef126251f9d0130e620c42724b0b0cf61296e40
Lucas Ojeda <lucasojedacasales@gmail.com>
2026-06-15 14:19:33 +0000

    File changes

--- src/lib/app-params.js
+++ src/lib/app-params.js
@@ -40,7 +40,7 @@ const getAppParams = () => {
 		storage.removeItem('token');
 	}
 	return {
-		appId: getAppParamValue("app_id", { defaultValue: import.meta.env.VITE_BASE44_APP_ID }),
+		appId: getAppParamValue("app_id", { defaultValue: import.meta.env.VITE_BASE44_APP_ID || "6a2e1f2e45b60383a960c225" }),
 		token: getAppParamValue("access_token", { removeFromUrl: true }),
 		fromUrl: getAppParamValue("from_url", { defaultValue: window.location.href }),
 		functionsVersion: getAppParamValue("functions_version", { defaultValue: import.meta.env.VITE_BASE44_FUNCTIONS_VERSION }),
